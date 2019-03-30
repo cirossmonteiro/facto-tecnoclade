2faec2799295e58f6a68a384ffbf732a8f0842c6
Ciro Monteiro <cirossmonteiro@gmail.com>
2019-03-30 02:03:00 -0300

    responsive grid
    media query

---
+++ react-app/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="All">
+          <div className="Row">
+            <div className="Module">
+              <div className="Module-Content">
+              
+              </div>
+            </div>
+            <div className="Module">
+              <div className="Module-Content">
+              
+              </div>
+            </div>
+          </div>
+          <div className="Row">
+          <div className="Module">
+              <div className="Module-Content">
+              
+              </div>
+            </div>
+            <div className="Module">
+              <div className="Module-Content">
+              
+              </div>
+            </div>
+          </div>
+        </div>:
+      </div>
+    );
+  }
+}
+
+export default App;
